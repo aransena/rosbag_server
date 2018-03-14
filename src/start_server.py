37c992f1f9469bad0968f69ca9076045d274388b
aransena <aransena@gmail.com>
2018-03-14 14:27:58 +0000

    ready notification

--- src/start_server.py
+++ src/start_server.py
@@ -6,4 +6,5 @@ from rosbag_server_classes import RobotRecordServer
 if __name__ == '__main__':
     rospy.init_node('rosbag_server')
     server = RobotRecordServer(rospy.get_name())
+    rospy.loginfo("rosbag server ready")
     rospy.spin()
