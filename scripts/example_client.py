ff7c5be97cf4fd90ee0baf232c322f21bde418d5
aransena <aransena@gmail.com>
2018-11-16 17:04:44 +0000

    Update example_client.py

--- scripts/example_client.py
+++ scripts/example_client.py
@@ -7,32 +7,26 @@ rostopic pub /test100 std_msgs/Empty -r 100
 rostopic pub /test1000 std_msgs/Empty -r 1000
 '''
 
-
-
 import rospy
-import time
 import os
 
 from rosbagClient import RosbagClient
 
-
+# Feedback callback
 def feedback(msg):
     print msg.status.split(',')
 
 
 if __name__ == '__main__':
-
     rospy.init_node('recording_test_client')
     client = RosbagClient('rosbag_server')
+    
     try:
-        start_t = time.time()
-
         home_folder = os.path.expanduser('~')
-
         result = client.record(home_folder+'/rosbag_server_data', "test", ["/test10", "/test100", "/test1000"], ['-l 100'],
                                feedback_callback=feedback)
 
         print result.outcome.split(',')
 
-    except rospy.ROSInterruptException:
-        print "Program interrupted"
+    except Exception as e:
+        print "Error: ", e
